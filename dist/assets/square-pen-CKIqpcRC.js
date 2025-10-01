9a49efdb171b78375a235dcf54dd470c44750c25
mms2209 <54174085+mms2209@users.noreply.github.com>
2025-10-01 15:00:07 +0400

    Publish application

--- dist/assets/square-pen-CKIqpcRC.js
+++ dist/assets/square-pen-CKIqpcRC.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Dfz98CRi.js";/**
+import{c as a}from"./index-WviYWK-B.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
